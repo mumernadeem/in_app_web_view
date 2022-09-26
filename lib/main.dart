cbff49413537a8625218513321bea1041ea19ba6
up46629 <up46629@gmail.com>
2022-09-26 14:20:56 +0500

    in app web view project

---
+++ lib/main.dart
@@ -0,0 +1,130 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Permission.camera.request();
+  // await Permission.microphone.request();
+  // await Permission.storage.request();
+
+  if (Platform.isAndroid) {
+    await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
+
+    var swAvailable = await AndroidWebViewFeature.isFeatureSupported(
+        AndroidWebViewFeature.SERVICE_WORKER_BASIC_USAGE);
+    var swInterceptAvailable = await AndroidWebViewFeature.isFeatureSupported(
+        AndroidWebViewFeature.SERVICE_WORKER_SHOULD_INTERCEPT_REQUEST);
+
+    if (swAvailable && swInterceptAvailable) {
+      AndroidServiceWorkerController serviceWorkerController =
+          AndroidServiceWorkerController.instance();
+
+      await serviceWorkerController
+          .setServiceWorkerClient(AndroidServiceWorkerClient(
+        shouldInterceptRequest: (request) async {
+          print(request);
+          return null;
+        },
+      ));
+    }
+  }
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'In App Web View',
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyChromeSafariBrowser extends ChromeSafariBrowser {
+  @override
+  void onOpened() {
+    print("ChromeSafari browser opened");
+  }
+
+  @override
+  void onCompletedInitialLoad() {
+    print("ChromeSafari browser initial load completed");
+  }
+
+  @override
+  void onClosed() {
+    print("ChromeSafari browser closed");
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({
+    super.key,
+  });
+  final ChromeSafariBrowser browser = MyChromeSafariBrowser();
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    rootBundle.load('assets/images/flutter-logo.png').then((actionButtonIcon) {
+      widget.browser.setActionButton(ChromeSafariBrowserActionButton(
+          id: 1,
+          description: 'Action Button description',
+          icon: actionButtonIcon.buffer.asUint8List(),
+          action: (url, title) {}));
+    });
+
+    widget.browser.addMenuItem(ChromeSafariBrowserMenuItem(
+        id: 2, label: 'Custom item menu 1', action: (url, title) {}));
+    widget.browser.addMenuItem(ChromeSafariBrowserMenuItem(
+        id: 3, label: 'Custom item menu 2', action: (url, title) {}));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Container(),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await widget.browser.open(
+            url: Uri.parse("https://usamamajid-46b08.web.app/"),
+            options: ChromeSafariBrowserClassOptions(
+              android: AndroidChromeCustomTabsOptions(
+                shareState: CustomTabsShareState.SHARE_STATE_OFF,
+                isSingleInstance: false,
+                isTrustedWebActivity: false,
+                keepAliveEnabled: true,
+              ),
+              ios: IOSSafariOptions(
+                  dismissButtonStyle: IOSSafariDismissButtonStyle.CLOSE,
+                  presentationStyle:
+                      IOSUIModalPresentationStyle.OVER_FULL_SCREEN),
+            ),
+          );
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
